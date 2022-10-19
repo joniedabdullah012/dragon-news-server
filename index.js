70e7a493fd5b6bfbccda150016c7417775f5bbd8
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-10-19 15:13:12 +0600

    backend-data

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express();
+
+const cors = require('cors');
+const port = process.env.PORT || 5000
+
+app.use(cors());
+
+const categories = require('./data/categories.json')
+app.get('/', (req, res) => {
+
+    res.send('news API Running');
+
+})
+
+app.get('/news-categories', (req, res) => {
+
+    res.send(categories);
+
+})
+
+app.listen(port, () => {
+    console.log('dragon news', port);
+})
+
+
